147017b86eb86f953f4ba06f591e070b319988c9
Phillip Hemleb <GitHub@phillip.hemleb.de>
2020-06-20 22:59:39 +0200

    Model Documentation added

--- app/Filter.php
+++ app/Filter.php
@@ -2,8 +2,31 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Filter
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $created_by
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Filter newModelQuery()
+ * @method static Builder|Filter newQuery()
+ * @method static Builder|Filter query()
+ * @method static Builder|Filter whereCreatedAt($value)
+ * @method static Builder|Filter whereCreatedBy($value)
+ * @method static Builder|Filter whereDescription($value)
+ * @method static Builder|Filter whereId($value)
+ * @method static Builder|Filter whereName($value)
+ * @method static Builder|Filter whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Filter extends Model
 {
     protected $attributes = [
--- app/Item.php
+++ app/Item.php
@@ -2,8 +2,33 @@
 
 namespace App;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Item
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $amount
+ * @property int $created_by
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Item newModelQuery()
+ * @method static Builder|Item newQuery()
+ * @method static Builder|Item query()
+ * @method static Builder|Item whereAmount($value)
+ * @method static Builder|Item whereCreatedAt($value)
+ * @method static Builder|Item whereCreatedBy($value)
+ * @method static Builder|Item whereDescription($value)
+ * @method static Builder|Item whereId($value)
+ * @method static Builder|Item whereName($value)
+ * @method static Builder|Item whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Item extends Model
 {
     protected $attributes = [
