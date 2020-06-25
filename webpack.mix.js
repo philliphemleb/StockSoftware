ac7463338730ce928ea0465cc9417a422ecd63f5
Phillip Hemleb <GitHub@phillip.hemleb.de>
2020-06-25 21:27:52 +0200

    Frontend Framework Bootstrap replaced with Tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+const tailwindcss = require('tailwindcss')
+
+mix.js('resources/js/app.js', 'public/js');
+
+mix.sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./resources/js/tailwind.config.js') ],
+    })
