17e4c384fe8ed3859d76c2aa9e2c278d274c32a5
Phillip Hemleb <GitHub@phillip.hemleb.de>
2020-06-27 17:56:31 +0200

    Sign out icon added to the navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
         <navigation>
             <navigation-list-item route="{{ route('home') }}" text="{{ __('navigation.index') }}" icon="fas fa-home"></navigation-list-item>
             <navigation-list-item route="{{ route('home') }}" text="{{ __('navigation.admin') }}" icon="fas fa-sliders-h"></navigation-list-item>
-            <navigation-list-item class="md:hidden" route="{{ route('logout') }}" text="{{ __('navigation.logout') }}" icon="fas fa-sliders-h"></navigation-list-item>
+            <navigation-list-item class="md:hidden" route="{{ route('logout') }}" text="{{ __('navigation.logout') }}" icon="fas fa-sign-out-alt"></navigation-list-item>
         </navigation>
 
 
