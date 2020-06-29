5701fd00378a1e9310eeeea38b56758828a9a7ad
Phillip Hemleb <GitHub@phillip.hemleb.de>
2020-06-29 18:03:08 +0200

    Item Translations added

---
+++ resources/lang/de/item.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'search_placeholder' => 'Lager durchsuchen...',
+    'name' => 'Name',
+    'amount' => 'Anzahl',
+    'action' => 'Aktion',
+    'delete_successful' => 'Erfolgreich gelöscht',
+    'delete_unsuccessful' => 'Konnte nicht gelöscht werden',
+];
