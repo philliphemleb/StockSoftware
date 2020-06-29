241fde82f0af02765038bd057124e86bcfbdcf00
Phillip Hemleb <GitHub@phillip.hemleb.de>
2020-06-29 18:02:50 +0200

    Navigation Background changed

--- resources/js/components/navigation/Navigation.vue
+++ resources/js/components/navigation/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="navigation" class="bg-gray-700 fixed w-screen bottom-0 md:w-auto md:inset-y-0 md:left-0">
+    <div id="navigation" class="bg-gray-900 fixed w-screen bottom-0 md:w-auto md:inset-y-0 md:left-0">
         <ul class="text-white grid grid-cols-3 md:grid-cols-1">
             <slot></slot>
         </ul>
