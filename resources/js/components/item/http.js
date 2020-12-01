aaddd21ceacc06dd7bd095a26a9bab77c3095b25
Phillip Hemleb <github@phillip.hemleb.de>
2020-12-01 21:11:35 +0100

    Fixed a bug that you can´t load items. There was a routing typo in item/http.js

--- resources/js/components/item/http.js
+++ resources/js/components/item/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = "http://localhost/StockSoftware/public/";
+const backendUrl = "http://localhost/StockSoftware/public";
 
 export default {
     async fetchItems(search, skip)
